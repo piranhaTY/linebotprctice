c24c639b2671afe4e773d96a34971c1a3f5daba0
piranhaTY <99316065+piranhaTY@users.noreply.github.com>
2022-02-10 14:33:50 +0800

    Update app.py
    update  getPlayStickerMessage()

--- app.py
+++ app.py
@@ -202,9 +202,9 @@ def getCallCarMessage(data):
 
 def getPlayStickerMessage():
     message = dict()
-    message['type']='sticker'
-    message['packageId']='789'
-    message['stickerId']='10871'
+    message["type"] = "sticker"
+    message["packageId"] = "789"
+    message["stickerId"] = "10871"
     return message
 
 
