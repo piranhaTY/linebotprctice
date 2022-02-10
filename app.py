e94f392d7646a6312bfc3b00b626b3ee1508e96a
piranhaTY <99316065+piranhaTY@users.noreply.github.com>
2022-02-10 19:11:37 +0800

    Update app.py
    update getLocationConfirmMessage

--- app.py
+++ app.py
@@ -196,6 +196,7 @@ def getCarouselMessage(data):
 
 
 def getLocationConfirmMessage(title, latitude, longitude):
+    data = {"title":title, "latitude":latitude, "longitud":longitud, "action":"get_near"}
     message = {
       "type": "template",
       "altText": "this is a confirm template",
@@ -203,6 +204,11 @@ def getLocationConfirmMessage(title, latitude, longitude):
           "type": "confirm",
           "text":  f"確認是否搜尋 {title} 附近地點？",
           "actions": [
+              {
+                "type":"postback",
+                "label":"是",
+                "data": json.dumps(data)
+              }
               {
                 "type": "message",
                 "label": "是",
