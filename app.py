710e970fe51f47df90686e93812756e889bd7268
piranhaTY <99316065+piranhaTY@users.noreply.github.com>
2022-02-10 15:25:37 +0800

    Update app.py
    update getImageMessage

--- app.py
+++ app.py
@@ -236,15 +236,15 @@ def getMRTSoundMessage():
 
 def getTaipei101ImageMessage(originalContentUrl=F"{end_point}/static/taipei_101.jpeg"):
     #originalContentUrl -- 利用Heroku讀取Local Side的圖, 也可用其他網上的URL(但會有無法掌控對方server狀況的風險)
-    message = dict()
-    message["type"] = "image" 
-    message["originalContentUrl"] = originalContentUrl
-    message["previewImageUrl"] = originalContentUrl
     return getImageMessage(originalContentUrl)
 
 
 def getImageMessage(originalContentUrl):
+    message = dict()    
     message = dict()
+    message["type"] = "image" 
+    message["originalContentUrl"] = originalContentUrl
+    message["previewImageUrl"] = originalContentUrl
     return message
 
 
