7af3fdbe8d357dd5e5dabd1ae3b85d96101ef706
piranhaTY <99316065+piranhaTY@users.noreply.github.com>
2022-02-19 21:00:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import json
 import configparser
 import os
 from urllib import parse
-# import mysql.connector as sql
+import mysql.connector as sql
 app = Flask(__name__, static_url_path='/static')
 UPLOAD_FOLDER = 'static'
 ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
